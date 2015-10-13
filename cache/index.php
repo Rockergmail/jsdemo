6fe053f467225766a96bd489b9909b5f5c895879
liuxiangrong <liuxiangrong@youmi.net>
2015-10-13 22:18:43 +0800

    cache

---
+++ cache/index.php
@@ -0,0 +1,12 @@
+<?php
+echo "rock"
+?>
+<html lang="en" manifest="test.manifest">
+<link rel='stylesheet' type='text/css' href='test.css'>
+</head>
+<body>
+<h1>Rock</h1>
+<p>this is test</p>
+<img src='1.jpg' />
+</body>
+</html>
